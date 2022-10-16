07caffbf4fe6a487f2e6343072982598c4f72298
apple <jamesshisia6@gmail.com>
2022-10-16 11:58:35 +0300

    New Submission form for Spring. Package: SubmissionForm Enabled Entering and viewing details

--- src/main/java/com/SumissionForm/FormController/Controller.java
+++ src/main/java/com/SumissionForm/FormController/Controller.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    //puts data to ViewCustomer.jsp
     @RequestMapping(value = "/customer",method = RequestMethod.GET)
     public String customerData(){
         System.out.println();
         return "Customer";
     }
 
-    //method puts data to ViewCustomer.jsp and displays it
+    //method  displays from ViewCustomer.jsp
     @RequestMapping(value = "/addCustomers",method = RequestMethod.POST)
     public String viewDetails(@RequestParam("cid") String cid,
                               @RequestParam("cname") String cname,
